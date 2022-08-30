cf28ca6a9690eacd918cb7635d4b8cb3279fd70e
CyberGiorgio <77939560+CyberGiorgio@users.noreply.github.com>
2022-08-30 21:38:42 +0200

    json update date and time

--- Colloquio/StaticClasses/JSONFile.cs
+++ Colloquio/StaticClasses/JSONFile.cs
@@ -14,47 +14,54 @@ namespace Colloquio.StaticClasses
 {
     static public class JSONFile
     {
-        public class Esame{
-            private string nomeEsame;       //attributes esame class
-            private string codiceMinisteriale;
-            private string codiceInterno;
-            private string descrizione;
-            private DateTime localDate;
-            public Esame()      //empty constructor
-            {
-            }
-            public Esame(string nomeEsame, string codiceMinisteriale, string codiceInterno, string descrizione, DateTime localDate)
-            {
-                this.nomeEsame = nomeEsame;
-                this.codiceMinisteriale = codiceMinisteriale;
-                this.codiceInterno = codiceInterno;
-                this.descrizione = descrizione;
-                this.localDate = localDate;
-            }
-            public string NomeEsame { get => nomeEsame; set => nomeEsame = value; }         //encapsulation
-            public string CodiceMinisteriale { get => codiceMinisteriale; set => codiceMinisteriale = value; }
-            public string CodiceInterno { get => codiceInterno; set => codiceInterno = value; }
-            public string Descrizione { get => descrizione; set => descrizione = value; }
-            public DateTime LocalDate { get => localDate; set => localDate = value; }
-        }
         public static void Serialize(ListView listViewEsamiAggiunti)        //serialize json objects into list
         {
+            TodayDate today = new TodayDate();
+            today.DateTime = DateTime.Today.ToString("dd-MM-yyyy");
+            today.Esami = new List<Esame>();
             List<Esame> listEsami = new List<Esame>();
             for (int i = 0; i < listViewEsamiAggiunti.Items.Count; i++)
             {
                     var esame = new Esame()
                     {
-                        LocalDate = DateTime.Now,
                         NomeEsame = listViewEsamiAggiunti.Items[i].SubItems[1].Text,
                         CodiceMinisteriale = listViewEsamiAggiunti.Items[i].SubItems[2].Text,
                         CodiceInterno = listViewEsamiAggiunti.Items[i].SubItems[3].Text,
                         Descrizione = listViewEsamiAggiunti.Items[i].SubItems[4].Text
                     };
-                    listEsami.Add(esame);
+                today.Esami.Add(esame);
             }
-            var json = JsonConvert.SerializeObject(listEsami);      //serialize list
-            string fileName = "esami.json";             
+            var json = JsonConvert.SerializeObject(today);      //serialize list
+            string fileName = "esami-"+ today.DateTime +".json";             
             File.WriteAllText(fileName, json);          //save file name with the name stated above
         }
+    }
+    public class TodayDate
+    {
+        string dateTime;
+        List<Esame> esami;
+        public string DateTime { get => dateTime; set => dateTime = value; }
+        public List<Esame> Esami { get => esami; set => esami = value; }
+    }
+    public class Esame
+    {
+        private string nomeEsame;       //attributes esame class
+        private string codiceMinisteriale;
+        private string codiceInterno;
+        private string descrizione;
+        public Esame()      //empty constructor
+        {
+        }
+        public Esame(string nomeEsame, string codiceMinisteriale, string codiceInterno, string descrizione)
+        {
+            this.nomeEsame = nomeEsame;
+            this.codiceMinisteriale = codiceMinisteriale;
+            this.codiceInterno = codiceInterno;
+            this.descrizione = descrizione;
+        }
+        public string NomeEsame { get => nomeEsame; set => nomeEsame = value; }         //encapsulation
+        public string CodiceMinisteriale { get => codiceMinisteriale; set => codiceMinisteriale = value; }
+        public string CodiceInterno { get => codiceInterno; set => codiceInterno = value; }
+        public string Descrizione { get => descrizione; set => descrizione = value; }
     }
 }
